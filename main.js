0f33afb7beac6f165dc396e62ba34e4e75cd4c52
Ashley <avromero@scl.edu.gt>
2024-09-13 12:12:32 -0600

    seccion 6 tarea

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { header } from "./componentes/header/header.js";
+import { seccionTareas } from "./componentes/componentes/itemtarea/itemtarea.js";
 
 let vinculo = document.querySelector(".root");
 
 vinculo.appendChild(header);
- 
+
+vinculo.appendChild(seccionTareas);
